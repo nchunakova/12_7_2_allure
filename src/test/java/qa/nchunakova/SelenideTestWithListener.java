a8485ff598d3bc1f02468361c494c3412567766f
Natalia Chunakova <n-chunakova@yandex.ru>
2022-05-03 21:42:08 +0400

    init commit

---
+++ src/test/java/qa/nchunakova/SelenideTestWithListener.java
@@ -0,0 +1,41 @@
+package qa.nchunakova;
+
+        import com.codeborne.selenide.Condition;
+        import com.codeborne.selenide.logevents.SelenideLogger;
+        import io.qameta.allure.Feature;
+        import io.qameta.allure.Owner;
+        import io.qameta.allure.Severity;
+        import io.qameta.allure.SeverityLevel;
+        import io.qameta.allure.Story;
+        import io.qameta.allure.selenide.AllureSelenide;
+        import org.junit.jupiter.api.DisplayName;
+        import org.junit.jupiter.api.Test;
+
+        import static com.codeborne.selenide.Selectors.withText;
+        import static com.codeborne.selenide.Selenide.$;
+        import static com.codeborne.selenide.Selenide.open;
+        import static org.openqa.selenium.By.linkText;
+        import static org.openqa.selenium.By.partialLinkText;
+
+        @Owner("nchunakova")
+        @Severity(SeverityLevel.BLOCKER)
+        @Feature("Проверка названия задачи в репозитории")
+        @Story("Просмотр созданных задач в репозитории")
+        public class SelenideTestWithListener {
+
+        @Test
+        @DisplayName("Selenide test with listener")
+        public void testGithubIssue() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+
+        open("https://github.com");
+
+        $(".header-search-input").click();
+        $(".header-search-input").sendKeys("nchunakova/12_7_2_allure");
+        $(".header-search-input").submit();
+
+        $(linkText("nchunakova/12_7_2_allure")).click();
+        $(partialLinkText("Issues")).click();
+        $(withText("New issue")).shouldHave(Condition.visible);
+        }
+}
